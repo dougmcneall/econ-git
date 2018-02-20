c3b292cce745c6ecd368c184d0490ffc2ba03b6f
dougmcneall <dougmcneall@gmail.com>
2018-02-20 10:44:37 +0000

    Refactored, reordered, added BIC, reproduced spreadsheet results.

--- tol2018.R
+++ tol2018.R
@@ -1,8 +1,11 @@
 # tol2018.R
+# Reproducing the statistical analysis of Tol (2018) "The economic impacts
+# of climate change"
+# Doug McNeall dougmcneall@gmail.com
 
-# NOTE need to refactor to remove intercepts!
 dat = read.table('https://raw.githubusercontent.com/dougmcneall/econ-git/master/tol2018.txt',
                  header = TRUE)
+
 
 tol_fitted = read.table('tol2018_fitted_values.txt', header = TRUE)
 
@@ -20,7 +23,7 @@ fit0 = lm(impact ~ 0 + warming)
 # fit a parabola
 fit1 = lm(impact ~ 0 + warming2) 
 # fit a simple polynomial
-fit2 = lm(impact ~ 0+ warming + warming2)
+fit2 = lm(impact ~ 0 + warming + warming2)
 # fit exponential
 fit3 = lm(impact ~ 0 + warmingexp)
 # fit double exponential
@@ -28,6 +31,9 @@ fit4 = lm(impact ~ 0 + warmingexpexp)
 # miss fit5 for labelling purposes :)
 fit6 = lm(impact ~ 0 + warming2 + warming6)
 fit7 = lm(impact ~ 0 + warming2 + warming7)
+# piecewise linear fit
+library(segmented)
+fitseg = segmented(fit0, seg.Z = ~ 0 + warming)
 
 warmingseq = seq(from = -1, to = 6.5, by = 0.1)
 
@@ -39,7 +45,15 @@ newdata = data.frame(warming = warmingseq,
                      warming7 = warmingseq^7
                      )
 
+pred0= predict(fit0, newdata = newdata, se.fit = TRUE)
 pred1 = predict(fit1, newdata = newdata, se.fit = TRUE)
+pred2= predict(fit2, newdata = newdata, se.fit = TRUE)
+pred3= predict(fit3, newdata = newdata, se.fit = TRUE)
+pred4= predict(fit4, newdata = newdata, se.fit = TRUE)
+pred6= predict(fit6, newdata = newdata, se.fit = TRUE)
+pred7= predict(fit7, newdata = newdata, se.fit = TRUE)
+
+
 plot(dat)
 lines(newdata$warming, pred1$fit)
 lines(newdata$warming, pred1$fit+(2*pred1$se.fit), lty = 'dashed')
@@ -55,9 +69,6 @@ for(i in 1:ncol(tol_fitted)){
   
 }
 
-# piecewise linear
-library(segmented)
-fitseg = segmented(fit0, seg.Z = ~0 + warming)
 predseg = predict(fitseg, newdata = newdata, se.fit = TRUE )
 
 pdf('segmented_vs_parabolic.pdf')
@@ -74,12 +85,7 @@ abline(h = 0)
 abline(v = 0)
 dev.off()
 
-pred0= predict(fit0, newdata = newdata, se.fit = TRUE )
-pred2= predict(fit2, newdata = newdata, se.fit = TRUE )
-pred3= predict(fit3, newdata = newdata, se.fit = TRUE )
-pred4= predict(fit4, newdata = newdata, se.fit = TRUE )
-pred6= predict(fit6, newdata = newdata, se.fit = TRUE )
-pred7= predict(fit7, newdata = newdata, se.fit = TRUE )
+
 
 # Plot all the predictions
 plot(dat)
@@ -102,6 +108,16 @@ rl = function(x){
 aicvec = rl(c(AIC(fitseg), AIC(fit0), AIC(fit1), AIC(fit2), AIC(fit3), AIC(fit4), AIC(fit6), AIC(fit7)))
 names(aicvec) = fitorder
 
+bicvec = rl(c(BIC(fitseg),BIC(fit0), BIC(fit1), BIC(fit2), BIC(fit3),
+              BIC(fit4), BIC(fit6), BIC(fit7)))
+names(bicvec) = fitorder
+
+# Relative likelihood ratio, calculated by aic, 
+# expressed compared to total
+raic = (aicvec/sum(aicvec)) * 100
+# Relative likelihood ratio, calculated by bic,
+# expressed compared to total
+rbic = (bicvec/sum(bicvec)) * 100
 
 # I think that this is how Tol is, in effect, doing his likelihood
 # ratio test. This isn't appropriate for non-nested models.
@@ -121,30 +137,29 @@ expl = c(exp(logLik(fitseg)), exp(logLik(fit0)), exp(logLik(fit1)), exp(logLik(f
 rexpl = (expl/sum(expl)) *100
 names(rexpl) = fitorder
 
-# Relative likelihood ratio, calculated by aic, expressed in the same way
-raic = (aicvec/sum(aicvec)) * 100
-
-cbind(rexpl, raic)
+round(cbind(rexpl, raic, rbic))
 
 # Check that AIC and loglikelihood are being used in the same way
-npar = 3
+# npar = length(coef(fit)) + 1, as I think AIC counts the estimated 
+# uncertainty as a parameter. This shouldn't matter, as it's the 
+# *difference* between number of parameters that counts in the AIC
+# comparison.
+
+npar = 2
 AIC(fit0)
 (-2*logLik(fit0)) + 2*npar
 
-npar = 3
+npar = 2
 AIC(fit1)
 (-2*logLik(fit1)) + 2*npar
 
-npar = 4
+npar = 3
 AIC(fit2)
 (-2*logLik(fit2)) + 2*npar
 
 AIC(fitseg)
-npar = 5
+npar = 4
 (-2*logLik(fitseg)) + 2*npar
-
-# Looks like the PWL fit has 5 parameters, which is why it is being more heavily
-# penalised by AIC measure.
 
 # hand calulate AIC using only residuals and number
 # of parameters in the model
@@ -154,7 +169,7 @@ calcAICdiff = function(fit){
   # I think this should be longer (i.e. include variance), but shouldn't
   # matter if we are just comparing AIC in similar models
   # i.e., it's the difference in parameters that matters.
-  k = length(coef(fit)) 
+  k = length(coef(fit)) # + 1
   n = length(fit$residuals)
   
   aic = 2*k + n*(log(rss))
@@ -174,16 +189,20 @@ testAIC = rl(c(calcAICdiff(fitseg),
 # These should be the same
 cbind(testAIC, aicvec)
 
+
 # Can we reproduce the likelihood (and relative likelihood)
 # Values from the spreadsheet?
-toll = function(fit){
+toll = function(fit, rez = NULL){
   # this is my attempt at backing out the likelihood formula
   # from the spreadsheet, but it doesn't seem to produce the 
   # same results.
+  if(is.null(rez)){
+    rez = fit$residuals
+  }
   
-  rss = sum(fit$residuals^2)
-  k = length(coef(fit)) -1 # don't count the intercept, apparently
-  n = length(fit$residuals)
+  rss = sum(rez^2)
+  k = length(coef(fit)) 
+  n = length(rez)
   ll = -(rss/2) /  (  (sqrt(rss/(n-k)))^2 - n*(log(sqrt(rss/(n-k)))))
   
   ll
@@ -196,11 +215,83 @@ rtol = (tollvec/sum(tollvec))*100
 names(rtol) = fitorder
 
 #table of the relative likelihood measures
-out = cbind(round(rtol, 3), round(raic, 3), round(rexpl, 3))
+out = round(cbind(rtol, rexpl, raic, rbic))
 
-colnames(out) = c('Tol_RL', 'AIC', 'R_RL')
+colnames(out) = c('Tol RL', 'RL', 'AIC RL', 'BIC RL')
 print(out)
 
+# Received email from R. Tol 16/02/2018
+#Likelihood for one observation = 1/sqrt(2pi)/sigma e^(-0.5((x-mu)/sigma)^2)
+
+#Take log
+#(x-mu)^2 is the squared residual
+#Loglikelihood for all observations is thus proportional to the sum of squared residuals
+#The maximum likelihood estimator of sigma^2 is the sum of squared residuals over the number of degrees of freedom
+#With an uniformative prior, the likelihood of the data given the model (calculated above) is proportional to the likelihood of the model given the data. For a countable set of candidate models, you rescale to sum to one.
+
+# Calculate sigma by hand, with info from Tol's email
+testfun = function(fit, rez=NULL){
+  
+  if(is.null(rez)){
+    rez = fit$residuals
+  }
+  
+ # if(is.null(rez)){
+  p = length(coef(fit))
+#  }
+
+  n = length(rez)
+  rss = sum(rez^2)
+  sigma = sqrt(rss/(n))
+  #ll = log(1/(sqrt(2*pi)/sigma) * exp(-0.5*(sum(fit$residuals)/sigma)^2))
+  #ll = log(1/sqrt(2*pi*sigma) * exp(-0.5*(sum(rez^2)/sigma^2)))
+  #ll = log
+  truell = -(n/2)*log(2*pi) - (n/2)*log(sigma^2) - (1/(2*sigma^2))*rss
+  truell
+}
+
+rtestfun = (testfunvec/sum(testfunvec))*100
+names(rtestfun) = fitorder
+
+RLtab = cbind(round(rtol), round(rexpl), round(rtestfun), round(raic))
+
+RLtab = round(cbind(rtestfun, rtol, rexpl, raic, rbic))
+rownames(RLtab) = fitorder
+print(RLtab)
+
+# Here's Tol's function working on the fits from Tol's own spreadsheet.
+tolfit = c(exp(toll(fitseg, rez = tol_fitted[,2] - impact)),
+           exp(toll(fit0, rez = tol_fitted[,8] - impact)),
+           exp(toll(fit1, rez = tol_fitted[,3] - impact)),
+           exp(toll(fit2, rez = tol_fitted[,1] - impact)),
+           exp(toll(fit3, rez = tol_fitted[,6] - impact)),
+           exp(toll(fit4, rez = tol_fitted[,7] - impact)),
+           exp(toll(fit6, rez = tol_fitted[,5] - impact)),
+           exp(toll(fit7, rez = tol_fitted[,4] - impact))
+)
+rtolfit = (tolfit/sum(tolfit))*100
+
+# Here's the first-principles likelihood calculation, working on
+# Tol's fitted values from the spreadsheet
+testfunvec_tol = c(exp(testfun(fitseg, rez = tol_fitted[,2] - impact)),
+                   exp(testfun(fit0, rez = tol_fitted[,8] - impact)),
+                   exp(testfun(fit1, rez = tol_fitted[,3] - impact)),
+                   exp(testfun(fit2, rez = tol_fitted[,1] - impact)),
+                   exp(testfun(fit3, rez = tol_fitted[,6] - impact)),
+                   exp(testfun(fit4, rez = tol_fitted[,7] - impact)),
+                   exp(testfun(fit6, rez = tol_fitted[,5] - impact)),
+                   exp(testfun(fit7, rez = tol_fitted[,4] - impact))
+)
+
+rtestfun_tol = (testfunvec_tol/sum(testfunvec_tol))*100
+
+#Same answers as Tol, but with parabolic/quadratic reversed?
+# HA! Finally found the mistake. Checked on the spreadsheet - Tol has mistakenly reversed
+# "parabolic" and "quadratic" labels in the final table. 
+# He calls cell AJ9 = W4 Quadratic (actually T^2) and AJ3 = S4 Parabolic (actually T +T^2)
+RL_tolfits = round(cbind(rtolfit, rtestfun_tol ))
+rownames(RL_tolfits) = fitorder
+print(RL_tolfits)
 
 
 # --------------------------------------------------------------------
